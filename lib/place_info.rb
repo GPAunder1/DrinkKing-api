8115c2132195319faf178c91a208ce456c0a3a9b
jxderulo <hanchenghan@gmail.com>
2020-10-17 02:41:05 +0800

    update place_info.rb which passes rubocop

--- lib/place_info.rb
+++ lib/place_info.rb
@@ -4,29 +4,32 @@ require 'http'
 require 'yaml'
 
 config = YAML.safe_load(File.read('../config/secrets.yml'))
-#print config['api_token']
+
+def api_type_picker(type)
+  case type
+  when 'place_findplace' then 'findplacefromtext'
+  when 'place_nearbysearch' then 'nearbysearch'
+  end
+end
 
 def api_path(config, type, path)
-  #case type:
-end
-def api_place_path(config, path)
-  "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=#{config['api_token']}&" + path
+  "https://maps.googleapis.com/maps/api/place/#{api_type_picker(type)}/json?key=#{config['api_token']}&" + path
 end
 
-def api_nearbyplaces_path(config, path)
-  "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{config['api_token']}&" + path
-end
-def call_api_url(config, url)
+def call_api_url(url)
   HTTP.get(url)
 end
 
-response = {}
 results = []
 
-findplace_url = api_place_path(config, 'input=國立清華大學&inputtype=textquery&language=zh-TW&fields=place_id')
-findnearbyplaces_url = api_nearbyplaces_path(config, 'location=24.7961217,120.9966699&radius=10&keyword=飲料&language=zh-TW')
-response = call_api_url(config, findnearbyplaces_url).parse #convert httpresponse to hash
-statuscode = response['status'] #ex: 200 OK
+findnearbyplaces_url = api_path(
+  config,
+  'place_nearbysearch',
+  'location=24.7961217,120.9966699&rankby=distance&keyword=飲料&language=zh-TW'
+)
+
+response = call_api_url(config, findnearbyplaces_url).parse # convert httpresponse to hash
+# response['status'] return the status code of response
 places = response['results']
 
 places.map do |place|
@@ -34,7 +37,7 @@ places.map do |place|
   result['name'] = place['name']
   result['address'] = place['vicinity']
   result['location'] = place['geometry']['location']
-  result['open_now'] = place['opening_hours']['open_now']
+  result['open_now'] = place['opening_hours']['open_now'] unless place['opening_hours'].nil?
   result['rating'] = place['rating']
   results << result
 end
