255a637d8125dd28aa288465ef52c64ef1a0ac03
timchen7201 <timchen.nba@gmail.com>
2020-10-21 14:21:44 +0800

    Adding SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
 
 require 'minitest/autorun'
 require 'minitest/rg'
@@ -7,10 +9,11 @@ require_relative '../lib/googlemap_api'
 require 'vcr'
 require 'webmock'
 
+
 GARBLE = 'dcnisndisncsdc'
 BAD_TOKEN = 'snidnsicndsivndsivdsv'
-TOKEN = YAML.safe_load(File.read('../config/secrets.yml'))['api_token']
-CORRECT = YAML.safe_load(File.read('./fixtures/googlemap_results.yml'))
+TOKEN = YAML.safe_load(File.read('config/secrets.yml'))['api_token']
+CORRECT = YAML.safe_load(File.read('spec/fixtures/googlemap_results.yml'))
 
-CASSETTES_FOLDER = './fixtures/cassettes'
+CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'googlemap_api'
