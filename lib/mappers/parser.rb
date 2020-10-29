f465878904275fd041b8a85754f7d6f87ac33610
jxderulo <hanchenghan@gmail.com>
2020-10-30 02:39:45 +0800

    rename nearbyplace to shop_mapper and update as mappers

--- lib/mappers/parser.rb
+++ lib/mappers/parser.rb
@@ -1,42 +1,16 @@
 # frozen_string_literal: true
 
 module CodePraise
-  # to prase common things about googlemapapi
-  module Prasers
-    # to pick_api
-    class ApiPicker
-      def initialize(category, type)
-        @category = category
-        @type = type
-      end
-
-      def pick
-        case @category
-        when 'place'
-          case @type
-          when 'findplace' then 'place/findplacefromtext'
-          when 'nearbysearch' then 'place/nearbysearch'
-          end
-        end
-      end
-    end
-
+  # to parse data from googlemapapi
+  module Parsers
     # parse response to detect whether output is places array or error string
-    class ApiResponseParser
+    class ApiResponse
       def initialize(response)
         @response = response
       end
 
-      def map_place
-        if @response.is_a? String
-          raise_error
-        else
-          @response['results'].map { |place_data| Nearbyplace.new(place_data) }
-        end
-      end
-
-      def raise_error
-        @response
+      def apierror?
+        @response.is_a?(String) ? false : true
       end
     end
   end
