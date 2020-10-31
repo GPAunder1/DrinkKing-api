9c55f96e575c5ea573eeadc685d518a63f7873cf
timchen7201 <timchen.nba@gmail.com>
2020-10-31 12:47:01 +0800

    Finish mvc architecture fitting our api

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -7,15 +7,30 @@ module CodePraise
   # Roda
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/views'
+    plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :halt
 
     route do |routing|
-
+      routing.assets
       routing.root do
         view 'index'
       end
 
-      # routing.on
+      routing.on 'shop' do
+        routing.is do
+          routing.post do
+            shop_name = routing.params['drinking_shop']
+            routing.redirect "shop/#{shop_name}"
+          end
+          # view 'shop'
+        end
+        routing.on String do |shop_name|
+          routing.get do
+            places = CodePraise::Googlemap::ShopMapper.new(TOKEN).find(shop_name)
+            view 'shop', locals: { shops: places }
+          end
+        end
+      end
     end
   end
 end
