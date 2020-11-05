dc958144243036ed30098e439291cf0d3554aac7
jxderulo <hanchenghan@gmail.com>
2020-11-06 05:11:46 +0800

    add and update mappers related to placedetails api

--- app/models/mappers/shop_mapper.rb
+++ app/models/mappers/shop_mapper.rb
@@ -8,17 +8,18 @@ module CodePraise
     class ShopMapper
       def initialize(token, gateway_class = Googlemap::Api)
         @token = token
-        @gateway = gateway_class.new(@token)
+        @gateway_class = gateway_class
+        @gateway = @gateway_class.new(@token)
       end
 
       def find(keyword)
         # future extension:current location
         data = @gateway.nearbyplaces_data(keyword, [24.7961217, 120.9966699])
-        Parsers::ApiResponse.new(data).apierror? ? ShopMapper.build_entity(data) : raise_error(data)
+        Parsers::ApiResponse.new(data).apierror? ? build_entity(data) : raise_error(data)
       end
 
-      def self.build_entity(data)
-        data.map { |place| DataMapper.new(place).build_entity }
+      def build_entity(data)
+        data.map { |place| DataMapper.new(place, @token, @gateway_class).build_entity }
       end
 
       def raise_error(error_string)
@@ -28,8 +29,10 @@ module CodePraise
 
     # Extracts entity specific elements from data structure
     class DataMapper
-      def initialize(data)
+      def initialize(data, token, gateway_class)
         @data = data
+        @shop_details_mapper = ShopDetailMapper.new(token, gateway_class)
+        @shop_details = @shop_details_mapper.find_by_placeid(@data['place_id'])
       end
 
       def build_entity
@@ -38,12 +41,16 @@ module CodePraise
           name: name,
           address: address,
           location: location,
+          phone_number: phone_number,
+          map_url: map_url,
           opening_now: opening_now,
-          rating: rating
+          rating: rating,
+          reviews: reviews
         )
       end
 
       private
+
       def id
         @data['place_id'] ||= nil
       end
@@ -53,7 +60,7 @@ module CodePraise
       end
 
       def address
-        @data['vicinity'] ||= nil
+        @shop_details['formatted_address'] ||= nil
       end
 
       def location
@@ -63,15 +70,28 @@ module CodePraise
         geometry['location']
       end
 
+      def phone_number
+        @shop_details['formatted_phone_number'] ||= nil
+      end
+
+      def map_url
+        @shop_details['url']
+      end
+
       def opening_now
         opening_hours = @data['opening_hours']
         return nil unless opening_hours
 
-        opening_hours['open_now'] ? "opening" : "closed"
+        opening_hours['open_now'] ? 'opening' : 'closed'
+        # future extension: add opening hours(already inside called api)
       end
 
       def rating
         @data['rating'] ||= nil
+      end
+
+      def reviews
+        ShopDetailMapper.build_review_entity(@shop_details['reviews'])
       end
     end
   end
