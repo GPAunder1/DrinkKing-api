5432792d28ecb338639a276508ae0cdd98b0e06e
jxderulo <hanchenghan@gmail.com>
2020-11-08 02:05:47 +0800

    update controllers to fetch data from database

--- app/models/repositories/shops.rb
+++ app/models/repositories/shops.rb
@@ -17,7 +17,9 @@ module CodePraise
       end
 
       def self.find_shop(shopname)
-        rebuild_entity Database::ShopOrm.first(name: shopname)
+        Database::ShopOrm.where(Sequel.like(:name, "#{shopname}%")).all.map do |shop|
+          rebuild_entity(shop)
+        end
       end
 
       def self.find_placeid(placeid)
@@ -25,12 +27,9 @@ module CodePraise
         rebuild_entity(db_record)
       end
 
-      def self.db_find_or_create(entity)
-        Database::ShopOrm.find_or_create(entity.to_attr_hash)
-      end
-
       def self.create(entity)
-        raise 'Shop already exists' if find(entity)
+        # raise 'Shop already exists' if find(entity)
+        return unless find(entity)
 
         db_shop = PersistShop.new(entity).call
         rebuild_entity(db_shop)
@@ -43,16 +42,6 @@ module CodePraise
           db_record.to_hash.merge(
             reviews: Reviews.rebuild_many(db_record.reviews)
           )
-          # id: db_record.id,
-          # placeid: db_record.placeid,
-          # name: db_record.name,
-          # address: db_record.address,
-          # location: db_record.location,
-          # phone_number: db_record.phone_number,
-          # map_url: db_record.map_url,
-          # opening_now: db_record.opening_now,
-          # rating: db_record.rating,
-          # reviews: db_record.reviews
         )
       end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ require_relative '../init'
 
 GARBLE = 'dcnisndisncsdc'
 BAD_TOKEN = 'snidnsicndsivndsivdsv'
-KEYWORD = '飲料'
+KEYWORD = '可不可'
 TOKEN = CodePraise::App.config.api_token
-# TOKEN = YAML.safe_load(File.read('config/secrets.yml'))['development']['api_token']
 CORRECT = YAML.safe_load(File.read('spec/fixtures/googlemap_results.yml'))
