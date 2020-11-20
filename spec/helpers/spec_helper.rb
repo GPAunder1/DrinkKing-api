0cc5199a2e2d4b2378cac1f8f0ac505eebda49bd
jxderulo <hanchenghan@gmail.com>
2020-11-21 01:04:26 +0800

    add acceptance test

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -10,10 +10,15 @@ require 'minitest/rg'
 require 'yaml'
 require 'vcr'
 require 'webmock'
-require_relative '../init'
+require_relative '../../init'
 
 GARBLE = 'dcnisndisncsdc'
 BAD_TOKEN = 'snidnsicndsivndsivdsv'
 KEYWORD = '可不可'
 TOKEN = CodePraise::App.config.API_TOKEN
 CORRECT = YAML.safe_load(File.read('spec/fixtures/googlemap_results.yml'))
+
+# Helper methods
+def homepage
+  CodePraise::App.config.APP_HOST
+end
