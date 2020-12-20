128756c9a85b9df9dfa77f7fbede0126b04a8e70
jxderulo <hanchenghan@gmail.com>
2020-12-20 16:39:10 +0800

    add recommend_drink column to database

--- app/domain/shops/entities/shop.rb
+++ app/domain/shops/entities/shop.rb
@@ -7,20 +7,21 @@ module DrinkKing
     class Shop < Dry::Struct
       include Dry.Types
 
-      attribute :id,            Strict::Integer.optional
-      attribute :placeid,       Strict::String
-      attribute :name,          Strict::String
-      attribute :address,       Strict::String
-      attribute :latitude,      Strict::Float
-      attribute :longitude,     Strict::Float
-      attribute :phone_number,  Strict::String.optional
-      attribute :map_url,       Strict::String
-      attribute :opening_now,   Strict::String.optional
-      attribute :rating,        Strict::Integer | Strict::Float
-      attribute :reviews,       Strict::Array.of(Review)
+      attribute :id,              Strict::Integer.optional
+      attribute :placeid,         Strict::String
+      attribute :name,            Strict::String
+      attribute :address,         Strict::String
+      attribute :latitude,        Strict::Float
+      attribute :longitude,       Strict::Float
+      attribute :phone_number,    Strict::String.optional
+      attribute :map_url,         Strict::String
+      attribute :opening_now,     Strict::String.optional
+      attribute :rating,          Strict::Integer | Strict::Float
+      attribute :reviews,         Strict::Array.of(Review)
+      attribute :recommend_drink, Strict::String.optional
 
       def to_attr_hash
-        to_hash.reject { |key, _| %i[id reviews].include? key }
+        to_hash.reject { |key, _| %i[id reviews recommend_drink].include? key }
       end
     end
   end
