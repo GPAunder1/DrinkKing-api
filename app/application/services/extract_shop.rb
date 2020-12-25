864eff7b60ce8fabb8b1d305ed4f856d52e22633
jxderulo <hanchenghan@gmail.com>
2020-12-26 02:34:46 +0800

    modify extract shop service

--- app/application/services/extract_shop.rb
+++ app/application/services/extract_shop.rb
@@ -10,19 +10,19 @@ module DrinkKing
     class ExtractShop
       include Dry::Monads::Result::Mixin
 
-      PROCESSING_MSG = 'Processing the request'
-
       def call(input)
         shopid = input[:shop_id]
         recommend_drink = Repository::Shops.find_recommend_drink(shopid)
-        return Success(Response::ApiResult.new(status: :ok, message: recommend_drink)) unless recommend_drink.empty?
+
+        # dirty code below , because recommend_drink is string... should be Success
+        return Failure(Response::ApiResult.new(status: :ok, message: recommend_drink)) unless recommend_drink.empty?
 
         # Success(Response::ApiResult.new(status: :ok, message: temp_recommend_drink))
         Messaging::Queue
           .new(App.config.EXTRACT_QUEUE_URL, App.config)
           .send(input[:shop_id])
 
-        Failure(Response::ApiResult.new(status: :processing, message: PROCESSING_MSG))
+        Failure(Response::ApiResult.new(status: :processing, message: { request_id: input[:request_id] }))
       end
     end
   end
