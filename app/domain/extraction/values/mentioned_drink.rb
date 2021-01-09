716eb5e69d4959b9cc57a0cd65a606aafa2d88e7
jxderulo <hanchenghan@gmail.com>
2021-01-10 00:57:42 +0800

    integrate with FB page

--- app/domain/extraction/values/mentioned_drink.rb
+++ app/domain/extraction/values/mentioned_drink.rb
@@ -9,10 +9,7 @@ module DrinkKing
   module Value
     class DrinkGrappler
       def initialize
-        file = File.read('./app/domain/extraction/values/drinks.json')
-        obj = JSON.parse(file)
-        drinks = obj['drinks']
-        @menu_list = drinks.map { |drink| drink['name'] }
+        # nothing to do right now
       end
       def mention_drink(characters)
         selected_mention_drink = characters.select { |_, pos| pos == 'DRINK_NOUN' }.keys[0]
