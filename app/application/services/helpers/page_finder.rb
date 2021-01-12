6da56ad166016d2eb1c7c0866ef89083c291a466
jxderulo <hanchenghan@gmail.com>
2021-01-12 23:04:19 +0800

    update promotion keyword

--- app/application/services/helpers/page_finder.rb
+++ app/application/services/helpers/page_finder.rb
@@ -33,7 +33,7 @@ module DrinkKing
 
       def new_drink
         @posts.reject do |shop|
-          new_post = shop['posts'].select { |post| post['text'].match? /新品|上市/ }
+          new_post = shop['posts'].select { |post| post['text'].match? /新品|上市|冬季/ }
           shop['posts'] = new_post
           shop['posts'].empty?
         end
